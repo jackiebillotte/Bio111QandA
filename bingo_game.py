c89510e73e13ddcc26836f2d6868791424b7ed05
Jackie Billotte <83250757+jackiebillotte@users.noreply.github.com>
2021-11-26 21:03:50 -0700

    Add files via upload

---
+++ bingo_game.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+import random
+import csv
+
+st.markdown(""" <style> .font {
+font-size:50px ; font-family: 'Cooper Black'; color: #FF9633;} 
+</style> """, unsafe_allow_html=True)
+st.markdown('<p class="font">BIO 111 Bingo Review</p>', unsafe_allow_html=True)
+
+
+#st.title("BIO 111 Final Review Bingo")
+
+
+
+#Read in data
+#import csv
+with open('bingo_key.csv', newline='') as csvfile:
+		dict = pd.read_csv(csvfile, header=None, index_col=0, squeeze=True).to_dict()
+	
+numbers = list(range(1, 40))
+def fun():
+	i= np.random.randint(1,40)
+	if i in numbers:
+		st.header("*%s*" %  dict.get(i))
+		numbers.remove(i)
+		return numbers
+	else:
+		st.write("Spin again")
+
+
+	
+if st.sidebar.button("Select Question"):
+	fun()
